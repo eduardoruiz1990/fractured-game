780ded81fd60c8a6ee56e39e0cbc9cc0f0c9ec84
eduardoruiz1990 <ruiz.guayo.eduardo@gmail.com>
2026-03-07 20:05:46 +0100

    lead pipe and spilled ink implemented

--- src/data/Manifestations.js
+++ src/data/Manifestations.js
@@ -1,6 +1,5 @@
 // src/data/Manifestations.js
 // The JSON dictionary of weapons and upgrades. 
-// Note: We removed the functions from here to keep data and logic separated.
 export const MANIFESTATIONS = {
     flashlight: { 
         id: 'flashlight', 
@@ -19,13 +18,32 @@ export const MANIFESTATIONS = {
         name: 'Adrenaline Spike', 
         desc: 'Increases movement speed & sanity res.', 
         maxLvl: 5 
+    },
+    lead_pipe: { 
+        id: 'lead_pipe', 
+        name: 'Heavy Lead Pipe', 
+        desc: 'Crushing 360-degree melee sweep.', 
+        maxLvl: 5 
+    },
+    spilled_ink: { 
+        id: 'spilled_ink', 
+        name: 'Spilled Ink', 
+        desc: 'Leaves a slowing, toxic trail behind you.', 
+        maxLvl: 5 
     }
 };
+
 export const SYNERGIES = {
     blinding_signal: {
         id: 'blinding_signal',
         name: 'The Blinding Signal',
         desc: 'Flashlight strobes violently. Confuses enemies, causing them to attack each other.',
         reqs: ['flashlight', 'static']
+    },
+    industrial_bleed: {
+        id: 'industrial_bleed',
+        name: 'Industrial Bleed',
+        desc: 'Pipe hits cause massive ink splatters, dealing AoE damage.',
+        reqs: ['lead_pipe', 'spilled_ink']
     }
 };
